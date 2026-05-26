e566a3c5988898de7a4eb3d8a74039147001ae0f
岡崎陽平 <rain1171110>
2026-05-27 07:54:36 +0900

    # 2026-05-27 成長ログ：TypeScript class（constructor / this / method）

--- Understanding-TS-adjusted-project/dist/app.js
+++ Understanding-TS-adjusted-project/dist/app.js
@@ -2,8 +2,13 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log("Department:" + this.name);
+    }
 }
 const accounting = new Department("Accounting");
-console.log(accounting);
+accounting.describe();
+const accountingCopy = { name: "Dummy", describe: accounting.describe };
+accountingCopy.describe();
 export {};
 //# sourceMappingURL=app.js.map
--- Understanding-TS-adjusted-project/src/app.ts
+++ Understanding-TS-adjusted-project/src/app.ts
@@ -4,8 +4,31 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department:" + this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = {name:"Dummy", describe: accounting.describe };
+
+accountingCopy.describe();
+
+// 練習問題
+// class Animal {
+//   type: string;
+
+//   constructor(t: string) {
+//     this.type = t;
+//   }
+// }
+
+// const dog = new Animal("Dog");
+// const cat = new Animal("Cat");
+
+// console.log(dog);
+// console.log(cat);
