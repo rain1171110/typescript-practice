82d79a78ce95c32b15a329ecbaf9634584bc08a4
岡崎陽平 <rain1171110>
2026-06-07 10:41:22 +0900

    TypeScript の interface / type / implements / optional property の整理

--- Understanding-TS-adjusted-project/dist/app.js
+++ Understanding-TS-adjusted-project/dist/app.js
@@ -1,11 +1,27 @@
-let user1;
-user1 = {
-    name: "Max",
-    age: 30,
-    greet(phrase) {
-        console.log(phrase + " " + this.name);
-    },
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
 };
+class Person {
+    constructor(n) {
+        this.age = 30;
+        if (n) {
+            this.name = n;
+        }
+    }
+    greet(phrase) {
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        }
+        else {
+            console.log("Hi!");
+        }
+    }
+}
+let user1;
+user1 = new Person();
+console.log(add(1, 2));
 user1.greet("Hello I am");
+console.log(user1);
 export {};
 //# sourceMappingURL=app.js.map
--- Understanding-TS-adjusted-project/src/app.ts
+++ Understanding-TS-adjusted-project/src/app.ts
@@ -1,19 +1,48 @@
-interface Person {
-  name: string;
-  age: number;
+// type AddFn = (a:number, b:number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name?: string;
+  age = 30;
 
-user1 = {
-  name: "Max",
-  age: 30,
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
-  },
-};
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
+  }
+}
 
+let user1: Greetable;
 
-user1.greet("Hello I am")
+user1 = new Person();
+
+console.log(add(1,2))
+
+user1.greet("Hello I am");
+console.log(user1);
+
