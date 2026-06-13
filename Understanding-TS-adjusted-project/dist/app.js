36d583fcb2934891d4e7b872d360b52da84740ec
岡崎陽平 <rain1171110>
2026-06-14 07:02:53 +0900

    **ジェネリクス制約と Utility Type で、型を安全に加工する**

--- Understanding-TS-adjusted-project/dist/app.js
+++ Understanding-TS-adjusted-project/dist/app.js
@@ -2,8 +2,52 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
-const mergedObj2 = merge({ name: "Max" }, { age: 30 });
 console.log(mergedObj);
-console.log(mergedObj2);
+function countAndDescribe(element) {
+    let descriptionText = "値がありません";
+    if (element.length > 0) {
+        descriptionText = "値は" + element.length + "個です。";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking"]));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+extractAndConvert({ name: "Max" }, "name");
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Data1");
+textStorage.addItem("Data2");
+textStorage.removeItem("Data1");
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+numberStorage.addItem(30);
+numberStorage.removeItem(30);
+numberStorage.addItem(40);
+console.log(numberStorage.getItems());
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const names = ["Max", "Anna"];
+console.log(createCourseGoal);
+console.log(names);
 export {};
 //# sourceMappingURL=app.js.map
