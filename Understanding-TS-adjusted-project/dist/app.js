20c4c30a0b6ca4b1e6c9f88a704bbbc732cde558
岡崎陽平 <rain1171110>
2026-06-27 06:56:17 +0900

    # 学習ログ：TypeScript プロジェクト管理アプリ 模範解答への移行

--- Understanding-TS-adjusted-project/dist/app.js
+++ Understanding-TS-adjusted-project/dist/app.js
@@ -1,35 +1,15 @@
-class Project {
-    constructor(t, d, m) {
-        this.title = t;
-        this.description = d;
-        this.manday = m;
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
-const projectInputTemplate = document.getElementById("project-input");
-const projectSingleTemplate = document.getElementById("single-project");
-const projectListTemplate = document.getElementById("project-list");
-const app = document.getElementById("app");
-const importedInputNode = document.importNode(projectInputTemplate.content, true);
-const importedListNode = document.importNode(projectListTemplate.content, true);
-app.appendChild(importedInputNode);
-app.appendChild(importedListNode);
-const listTitle = document.querySelector(".projects h2");
-listTitle.textContent = "有効なプロジェクト";
-const projectList = document.querySelector(".projects ul");
-const projectForm = document.querySelector("form");
-projectForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const titleEl = document.getElementById("title");
-    const descriptionEL = document.getElementById("description");
-    const mandayEL = document.getElementById("manday");
-    const title = titleEl.value;
-    const description = descriptionEL.value;
-    const manday = +mandayEL.value;
-    const createdProject = new Project(title, description, manday);
-    const importedProjectNode = document.importNode(projectSingleTemplate.content, true);
-    const projectItem = importedProjectNode.firstElementChild;
-    projectItem.textContent = createdProject.title;
-    projectList.appendChild(projectItem);
-});
+const prjInput = new ProjectInput();
 export {};
 //# sourceMappingURL=app.js.map
--- Understanding-TS-adjusted-project/src/app.ts
+++ Understanding-TS-adjusted-project/src/app.ts
@@ -1,69 +1,162 @@
-class Project {
-  title: string;
-  description: string;
-  manday: number;
+// interface validatorConfig {
+//   [prop: string]: {
+//     [validatableProp: string]: string[];
+//   };
+// }
 
-  constructor(t: string, d: string, m: number) {
-    this.title = t;
-    this.description = d;
-    this.manday = m;
+// const registeredValidators: validatorConfig = {};
+
+// function Required(target: any, propName: string) {
+//   registeredValidators[target.constructor.name] = {
+//     ...registeredValidators[target.constructor.name],
+//     [propName]: [
+//       ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+//       "required",
+//     ],
+//   };
+// }
+
+// function PositiveNumber(target: any, propName: string) {
+//   registeredValidators[target.constructor.name] = {
+//     ...registeredValidators[target.constructor.name],
+//     [propName]: [
+//       ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+//       "positive",
+//     ],
+//   };
+// }
+
+// function validate(obj: any) {
+//   const objValidatorConfig = registeredValidators[obj.constructor.name];
+
+//   if (!objValidatorConfig) {
+//     return true;
+//   }
+
+//   let isValid = true;
+
+//   for (const prop in objValidatorConfig) {
+//     const validators = objValidatorConfig[prop];
+
+//     if (!validators) {
+//       continue;
+//     }
+
+//     for (const validator of validators) {
+//       switch (validator) {
+//         case "required":
+//           isValid = isValid && !!obj[prop];
+//           break;
+//         case "positive":
+//           isValid = isValid && obj[prop] > 0;
+//           break;
+//       }
+//     }
+//   }
+//   return isValid;
+// }
+
+// class Project {
+//   @Required
+//   title: string;
+//   @Required
+//   description: string;
+//   @PositiveNumber
+//   manday: number;
+
+//   constructor(t: string, d: string, m: number) {
+//     this.title = t;
+//     this.description = d;
+//     this.manday = m;
+//   }
+// }
+
+// const projectInputTemplate = document.getElementById(
+//   "project-input",
+// ) as HTMLTemplateElement;
+// const projectSingleTemplate = document.getElementById(
+//   "single-project",
+// ) as HTMLTemplateElement;
+// const projectListTemplate = document.getElementById(
+//   "project-list",
+// ) as HTMLTemplateElement;
+
+// const app = document.getElementById("app") as HTMLDivElement;
+
+// const importedInputNode = document.importNode(
+//   projectInputTemplate.content,
+//   true,
+// );
+
+// const importedListNode = document.importNode(projectListTemplate.content, true);
+
+// app.appendChild(importedInputNode);
+// app.appendChild(importedListNode);
+
+// const listTitle = document.querySelector(".projects h2")!;
+// listTitle.textContent = "有効なプロジェクト";
+// const projectList = document.querySelector(".projects ul")!;
+
+// const projectForm = document.querySelector("form")!;
+// projectForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const titleEl = document.getElementById("title") as HTMLInputElement;
+//   const descriptionEl = document.getElementById(
+//     "description",
+//   ) as HTMLTextAreaElement;
+//   const mandayEL = document.getElementById("manday") as HTMLInputElement;
+
+//   const title = titleEl.value;
+//   const description = descriptionEl.value;
+//   const manday = +mandayEL.value;
+
+//   const createdProject = new Project(title, description, manday);
+
+//   if (!validate(createdProject)) {
+//     alert("正しく入力して下さい");
+//     return;
+//   }
+
+//   const importedProjectNode = document.importNode(
+//     projectSingleTemplate.content,
+//     true,
+//   );
+
+//   const projectItem = importedProjectNode.firstElementChild as HTMLElement;
+//   projectItem.querySelector("h2")!.textContent = createdProject.title;
+
+//   const paragraphs = projectItem.querySelectorAll("p");
+//   paragraphs[0]!.textContent = createdProject.description;
+//   paragraphs[1]!.textContent = `${createdProject.manday}人日`;
+
+//   projectList.appendChild(projectItem);
+
+//   projectForm.reset();
+// });
+
+//模範解答-------------------------------------------------------------------------------------
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element:HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input",
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content,true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin",this.element);
   }
 }
 
-const projectInputTemplate = document.getElementById(
-  "project-input",
-) as HTMLTemplateElement;
-const projectSingleTemplate = document.getElementById(
-  "single-project",
-) as HTMLTemplateElement;
-const projectListTemplate = document.getElementById(
-  "project-list",
-) as HTMLTemplateElement;
-
-const app = document.getElementById("app") as HTMLDivElement;
-
-const importedInputNode = document.importNode(
-  projectInputTemplate.content,
-  true,
-);
-
-const importedListNode = document.importNode(projectListTemplate.content, true);
-
-app.appendChild(importedInputNode);
-app.appendChild(importedListNode);
-
-const listTitle = document.querySelector(".projects h2")!;
-listTitle.textContent = "有効なプロジェクト";
-const projectList = document.querySelector(".projects ul")!;
-
-const projectForm = document.querySelector("form")!;
-projectForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const titleEl = document.getElementById("title") as HTMLInputElement;
-  const descriptionEL = document.getElementById(
-    "description",
-  ) as HTMLTextAreaElement;
-  const mandayEL = document.getElementById("manday") as HTMLInputElement;
-
-  const title = titleEl.value;
-  const description = descriptionEL.value;
-  const manday = +mandayEL.value;
-
-  const createdProject = new Project(title, description, manday);
-
-  const importedProjectNode = document.importNode(
-    projectSingleTemplate.content,
-    true,
-  );
-
-  const projectItem = importedProjectNode.firstElementChild as HTMLElement;
-
-  projectItem.textContent = createdProject.title;
-  projectList.appendChild(projectItem);
-
-  //   if (!validate(createdProject)) {
-  //     alert("正しく入力して下さい");
-  //     return;
-  //   }
-  //   console.log(createdProject);
-});
+const prjInput = new ProjectInput();
